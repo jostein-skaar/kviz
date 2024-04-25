68ecfc6716004cc44c5fad2b226759c62c475000
Jostein Skaar <jostein.skaar@gmail.com>
2024-04-26 00:15:25 +0200

    feat: add schema and first question version

---
+++ src/lib/Question.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let isOpen = false;
+	export function closeModal() {
+		isOpen = false;
+	}
+
+	function onKeyDown(event: KeyboardEvent) {
+		// console.log(event);
+		if (event.code === 'Escape') {
+			closeModal();
+		}
+	}
+</script>
+
+<svelte:window on:keydown={onKeyDown} />
+
+{#if isOpen}
+	<div class="fixed inset-0 bg-black bg-opacity-80 z-50 flex justify-center items-start pt-40">
+		<div class="bg-white p-8 rounded-lg shadow-lg w-[800px]">
+			<button class="absolute top-0 right-0 p-4 text-xl" on:click={closeModal}>×</button>
+			<slot></slot>
+		</div>
+	</div>
+{/if}
