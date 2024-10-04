f9a4895fb8f32d0f33422f52bbe00fcbc4e96697
Jostein Skaar <jostein.skaar@gmail.com>
2024-10-04 12:06:16 +0200

    ci: try with npm, try 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import azure from 'svelte-adapter-azure-swa';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import azure from "svelte-adapter-azure-swa";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +11,30 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: azure()
-	}
+		adapter: azure({
+			customStaticWebAppConfig: {
+				routes: [
+					{
+						route: "/login",
+						rewrite: "/.auth/login/google",
+					},
+					{
+						route: "/logout",
+						rewrite: "/.auth/logout",
+					},
+				],
+				responseOverrides: {
+					401: {
+						redirect: "/login",
+						statusCode: 302,
+					},
+				},
+				platform: {
+					apiRuntime: "node:20",
+				},
+			},
+		}),
+	},
 };
 
 export default config;
